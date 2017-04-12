d990f3380cfefee0277c4f173f60d4e32bced615
Marco A L Barbosa <malbarbo@gmail.com>
2017-04-12 14:16:43 -0300

    Add termux support

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,7 @@ pub fn find_certs_dirs() -> Vec<PathBuf> {
         "/etc/openssl",
         "/etc/pki/tls",
         "/etc/ssl",
+        "/data/data/com.termux/files/usr/etc/tls",
     ].iter().map(|s| PathBuf::from(*s)).filter(|p| {
         fs::metadata(p).is_ok()
     }).collect()
